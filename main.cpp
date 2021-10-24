e5a8a98324f6c64ade730dadfc15a2d7b278738d
_kruss <80170798+krussmade@users.noreply.github.com>
2021-10-24 23:58:36 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -22,17 +22,42 @@ void errMessage2() {
 }
 
 int main(int argc, char *argv[]) {
+//======================================================================================================================
+    if (argc != 5) {
+        errMessage1();
+        return 1;
+    }
+    std::cout << "Start\n";
+
     int capacity;
     container *cont;
 
-    FILE *in = fopen("C:\\Users\\tagir\\Desktop\\a.txt", "r");
-    fscanf(in, "%d\n", &capacity);
-    cont = new container(capacity);
-    cont->fill(in);
-    fclose(in);
+    if (!strcmp(argv[1], "-f")) {
+        FILE *in = fopen(argv[2], "r");
+        fscanf(in, "%d\n", &capacity);
+        cont = new container(capacity);
+        cont->fill(in);
+        fclose(in);
+    } else {
+        if (!strcmp(argv[1], "-n")) {
+            capacity = atoi(argv[2]);
+            if ((capacity < 1) || (capacity > 10000)) {
+                std::cout << "incorrect numer of figures = "
+                          << capacity
+                          << ". Set 0 < number <= 10000\n";
+                return 3;
+            }
+            srand(static_cast<unsigned int>(time(0)));
+            cont = new container(capacity);
+            cont->fill();
+        } else {
+            errMessage2();
+            return 2;
+        }
+    }
 
-    FILE *out1 = fopen("C:\\Users\\tagir\\Desktop\\b.txt", "w");
-    FILE *out2 = fopen("C:\\Users\\tagir\\Desktop\\c.txt", "w");
+    FILE *out1 = fopen(argv[3], "w");
+    FILE *out2 = fopen(argv[4], "w");
 
     fprintf(out1, "Filled container:\n");
     cont->out(out1);
@@ -48,57 +73,4 @@ int main(int argc, char *argv[]) {
     fclose(out1);
     fclose(out2);
     return 0;
-
-
-//======================================================================================================================
-//    if (argc != 5) {
-//        errMessage1();
-//        return 1;
-//    }
-//    std::cout << "Start\n";
-//
-//    int capacity;
-//    container *cont;
-//
-//    if (!strcmp(argv[1], "-f")) {
-//        FILE *in = fopen(argv[2], "r");
-//        fscanf(in, "%d\n", &capacity);
-//        cont = new container(capacity);
-//        cont->fill(in);
-//        fclose(in);
-//    } else {
-//        if (!strcmp(argv[1], "-n")) {
-//            capacity = atoi(argv[2]);
-//            if ((capacity < 1) || (capacity > 10000)) {
-//                std::cout << "incorrect numer of figures = "
-//                          << capacity
-//                          << ". Set 0 < number <= 10000\n";
-//                return 3;
-//            }
-//            srand(static_cast<unsigned int>(time(0)));
-//            cont = new container(capacity);
-//            cont->fill();
-//        } else {
-//            errMessage2();
-//            return 2;
-//        }
-//    }
-//
-//    FILE *out1 = fopen(argv[3], "w");
-//    FILE *out2 = fopen(argv[4], "w");
-//
-//    fprintf(out1, "Filled container:\n");
-//    cont->out(out1);
-//
-//    cont->heapSort();
-//
-//    fprintf(out2, "Sorted container complete:\n");
-//    cont->out(out2);
-//
-//    delete cont;
-//    printf("Stop\n");
-//
-//    fclose(out1);
-//    fclose(out2);
-//    return 0;
 }
